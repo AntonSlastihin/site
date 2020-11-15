8808680e677c1121baddc7e60540d9e74f1221f9
Anton <toni.slastikhin@mail.ru>
2020-11-15 13:27:23 +0600

    added new files

--- application/views/news/create.php
+++
@@ -1,6 +0,0 @@
-<form action="/site/index.php/news/create" method="POST">
-	<input type="text" name="slug" placeholder="slug"> <br>
-	<input type="text" name="title" placeholder="Название новости"> <br>
-	<textarea name="text" placeholder="текст новости"></textarea><br>
-	<input type="submit" value="Отправить" name="submit">
-</form>
--- application/views/news/edit.php
+++
@@ -1,7 +0,0 @@
-<form action="/site/index.php/news/edit/<?= $slug_news ?>" method="POST">
-	<input type="text" name="slug" placeholder="slug" value="<?= $slug_news ?>"> <br>
-	<input type="text" name="title" placeholder="Название новости" value="<?= $title_news ?>"> <br>
-	<textarea name="text" placeholder="текст новости"><?= trim($content_news) ?></textarea><br>
-	<input type="hidden" value="<?= $id_news ?>" name="id">
-	<input type="submit" value="Отправить" name="submit">
-</form>
